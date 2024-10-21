e8103cd0986019f11eacdd82eb00ab0ff1a58d35
Pedro Henrique Melo Araujo <pedromeloaraujo1999@gmail.com>
2024-10-21 19:28:06 -0300

    Refactor main.cpp to use VibrationSystem instead of AcquisitionSubsystem, CommunicationSubsystem, and ProcessingSubsystem

--- main/main.cpp
+++ main/main.cpp
@@ -1,22 +1,13 @@
 #include "Arduino.h"
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
-#include "AcquisitionSubsystem.hpp"
-#include "CommunicationSubsystem.hpp"
-#include "ProcessingSubsystem.hpp"
+#include "VibrationSystem.hpp"
+#include "StartingDevice.hpp"
 
 extern "C" void app_main()
 {
-    AcquisitionSubsystem acquisition_subsystem;
-    CommunicationSubsystem communication_subsystem;
-    ProcessingSubsystem processing_subsystem;
-
-    acquisition_subsystem.init();
-    communication_subsystem.init();
-
-    acquisition_subsystem.startAcquisition();
-    communication_subsystem.startCommunication();
-    processing_subsystem.startProcessing();
+    VibrationSystem vibration_system;
+    vibration_system.init(new StartingDevice());
 
     while (true)
     {
