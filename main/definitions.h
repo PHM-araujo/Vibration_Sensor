746f3ab24f5a8612c5694ff5e6fe9b5a777b6bc7
Pedro Henrique Melo Araujo <pedromeloaraujo1999@gmail.com>
2024-10-24 15:48:06 -0300

    Remove SSID and PASS from definitions.h

--- main/definitions.h
+++ main/definitions.h
@@ -1,7 +1,5 @@
 #define ADXL345_SDA 33
 #define ADXL345_SCL 32
 #define ADXL345_INT2 35
-#define SSID "RepublicaDaBahia2G"
-#define PASS "LorenaMacetadora"
 #define TOKEN "ndpg9fop816t46snf2sl"
 #define THINGSBOARD_SERVER "thingsboard.cloud"
