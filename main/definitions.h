244a4532f5fbdc45a3627f47b5a080d164b86804
Pedro Henrique Melo Araujo <pedromeloaraujo1999@gmail.com>
2024-10-26 15:43:48 -0300

    Refactor definitions.h to update TOKEN and THINGSBOARD_SERVER values

--- main/definitions.h
+++ main/definitions.h
@@ -1,5 +1,5 @@
 #define ADXL345_SDA 33
 #define ADXL345_SCL 32
 #define ADXL345_INT2 35
-#define TOKEN "ndpg9fop816t46snf2sl"
-#define THINGSBOARD_SERVER "thingsboard.cloud"
+#define TOKEN "2GloTETDaJbJYBA8J8Av"
+#define THINGSBOARD_SERVER "150.162.216.231"
